805eba15f5b3da698f2a2c6ccda2006e42373f7f
Andreas Heimann <andreas@iklikla.de>
2017-11-09 17:34:39 +0100

    Seite fertig
    * Finished Homepage
    * Finished About
    * Finished Participate
    * Finished Contact
    * Finished Legal
    * Finished Data Privacy

--- docs/assets/js/headerFix.js
+++ docs/assets/js/headerFix.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var headerHeight = $("header").height();
   $(".header-fix").height(headerHeight);
-  
+
   $("#openMobileMenu").click(function() {
     $(".mobile-offcanvas").addClass('show');
     $(".mobile-offcanvas").animate({opacity: 1}, 150);
@@ -10,5 +10,24 @@ $(document).ready(function() {
     $(".mobile-offcanvas").animate({opacity: 0}, 150, function() {
       $(".mobile-offcanvas").removeClass('show');
     });
+  });
+  $(".mobile-offcanvas a").click(function() {
+    $(".mobile-offcanvas").animate({opacity: 0}, 150, function() {
+      $(".mobile-offcanvas").removeClass('show');
+    });
+  });
+
+  $(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+    if($.attr(this, 'href') == "#") {
+      var heightAnchor = 0;
+    } else {
+      var heightAnchor = $($.attr(this, 'href')).offset().top;
+    }
+    var correctHeight = heightAnchor - headerHeight - 25;
+
+    $('html, body').animate({
+        scrollTop: correctHeight
+    }, 500);
   });
 });
